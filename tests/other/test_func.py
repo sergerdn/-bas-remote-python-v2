ad08c9610ae8f87df245913bd70847030a451394
sergerdn <64213648+sergerdn@users.noreply.github.com>
2022-09-18 16:22:50 +0000

    feat: improve tests

--- tests/other/test_func.py
+++ tests/other/test_func.py
@@ -15,17 +15,14 @@ from bas_remote.runners import BasThread
 
 @pytest.mark.asyncio
 class TestFuncMultiple:
-    @pytest.mark.skip("skipped")
     async def test_function_check_ip(self, client_thread: BasThread):
         result = await client_thread.run_function("CheckIp")
         print(result)
 
-    @pytest.mark.skip("skipped")
     async def test_function_check_ip_json(self, client_thread: BasThread):
         result = await client_thread.run_function("CheckIpJson")
         print(result)
 
-    @pytest.mark.skip("skipped")
     async def test_function_return_big_data(self, client_thread: BasThread):
         data = await client_thread.run_function("TestReturnBigData")
         data_obj = yaml.load(data, Loader=yaml.UnsafeLoader)
@@ -45,7 +42,6 @@ class TestFuncMultiple:
                 ]
             ) == sorted(one.keys())
 
-    @pytest.mark.skip("skipped")
     @pytest.mark.timeout(timeout=60 * 3)
     async def test_function_task_canceled_error(
         self, client_options: Options, event_loop: asyncio.AbstractEventLoop, mocker: MockerFixture
@@ -76,7 +72,7 @@ class TestFuncMultiple:
         with pytest.raises(asyncio.exceptions.CancelledError):
             await thread.run_function("TestReturnBigData")
 
-    @pytest.mark.timeout(timeout=60)
+    @pytest.mark.timeout(timeout=60 * 3)
     async def test_function_process_killed(self, client_options: Options, event_loop: asyncio.AbstractEventLoop):
         client = BasRemoteClient(
             options=client_options,
