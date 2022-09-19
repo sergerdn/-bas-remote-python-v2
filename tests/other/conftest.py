58ac2e8a484d0a00dc8e48a65bb1515554d2d826
sergerdn <64213648+sergerdn@users.noreply.github.com>
2022-09-19 13:34:12 +0000

    fix: set_exception_handler

--- tests/other/conftest.py
+++ tests/other/conftest.py
@@ -28,30 +28,23 @@ def working_dir():
     return test_app_working_dir
 
 
-def get_new_event_loop() -> asyncio.AbstractEventLoop:
-    """Create an instance of the default event loop for each test case."""
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    loop.set_debug(True)
-    return loop
-
-
 @pytest.fixture(scope="function")
 def event_loop() -> asyncio.AbstractEventLoop:
     """Create an instance of the default event loop for each test case."""
-    loop = get_new_event_loop()
+    loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
-    if not loop.is_closed():
-        loop.close()
-    loop.stop()
+    if loop.is_closed():
+        return
+    loop.close()
 
 
 @pytest.fixture(scope="function")
 def client_options(
-    event_loop: asyncio.AbstractEventLoop,
-    working_dir,
-    remote_script_name,
-    remote_script_user,
-    remote_script_password,
+        event_loop: asyncio.AbstractEventLoop,
+        working_dir,
+        remote_script_name,
+        remote_script_user,
+        remote_script_password,
 ) -> Options:
     yield Options(
         script_name=remote_script_name,
@@ -63,9 +56,9 @@ def client_options(
 
 @pytest.fixture(scope="function")
 def client_thread(
-    client_options,
+        event_loop: asyncio.AbstractEventLoop,
+        client_options,
 ) -> BasThread:
-    event_loop = get_new_event_loop()
     client = BasRemoteClient(
         options=client_options,
         loop=event_loop,
@@ -78,6 +71,3 @@ def client_thread(
 
     event_loop.run_until_complete(thread.stop())
     event_loop.run_until_complete(client.close())
-    if not event_loop.is_closed():
-        event_loop.close()
-    event_loop.stop()
